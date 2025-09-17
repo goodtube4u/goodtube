f5977e6778f517d32be8c549cafdac1dc089b349
goodtube4u <goodtube4u@hotmail.com>
2025-09-17 13:52:44 +1000

    2x speed UI overlay

--- goodtube.js
+++ goodtube.js
@@ -3238,7 +3238,7 @@
 					<div class="ytp-speedmaster-label">2x</div>
 					<div class="ytp-speedmaster-icon">
 						<svg height="100%" version="1.1" viewBox="0 0 36 36" width="100%">
-							<use class="ytp-svg-shadow" xlink:href="#ytp-id-1"></use><path class="ytp-svg-fill" d="M 10,24 18.5,18 10,12 V 24 z M 19,12 V 24 L 27.5,18 19,12 z" id="ytp-id-1"></path>
+							<path class="ytp-svg-fill" d="M 10,24 18.5,18 10,12 V 24 z M 19,12 V 24 L 27.5,18 19,12 z" id="ytp-id-1"></path>
 						</svg>
 					</div>
 				</div>
