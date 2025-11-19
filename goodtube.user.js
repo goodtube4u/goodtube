7e3bfb57e9b8c5803064f46f01a3c1cfc450bfd9
londonchessmaster1-glitch <londonchessmaster1@gmail.com>
2025-11-19 16:09:43 +0100

    CSP updates

--- goodtube.user.js
+++ goodtube.user.js
@@ -38,58 +38,60 @@
 
 
 (function () {
-	'use strict';
-
-
+    'use strict';
 	/* Configure CSP
 	---------------------------------------------------------------------------------------------------- */
+	// Create a custom CSP policy
+	let goodTube_csp = false;
 	if (window.trustedTypes && window.trustedTypes.createPolicy) {
-		window.trustedTypes.createPolicy("default", {
-			createHTML: string => string,
-			createScriptURL: string => string,
-			createScript: string => string
+		goodTube_csp = window.trustedTypes.createPolicy("GoodTubePolicy", {
+            createScript: s => s
 		});
 	}
 
+    /* Load GoodTube with hash verification
+    ---------------------------------------------------------------------------------------------------- */
+    async function goodTube_load(loadAttempts = 0) {
+        // Only re-attempt to load the GoodTube 10 times
+        if (loadAttempts >= 9) {
+            if (window.top === window.self) {
+                console.log('[GoodTube] GoodTube could not be loaded');
+                alert('GoodTube could not be loaded! Please refresh the page to try again.');
+            }
+            return;
+        }
 
-	/* Load GoodTube
-	---------------------------------------------------------------------------------------------------- */
-	function goodTube_load(loadAttempts = 0) {
-		// Only re-attempt to load the GoodTube 10 times
-		if (loadAttempts >= 9) {
-			if (window.top === window.self) {
-				// Debug message
-				console.log('[GoodTube] GoodTube could not be loaded');
+        // Increment the load attempts
+        loadAttempts++;
 
-				// Show prompt
-				alert('GoodTube could not be loaded! Please refresh the page to try again.');
-			}
+        try {
+            const response = await fetch('https://raw.githubusercontent.com/goodtube4u/goodtube/refs/heads/main/goodtube.min.js');
+            const data = await response.text();
 
-			return;
-		}
+            // Verify script hash
+            const expectedHash = 'sha256-kPAWZtY3UZGsWD8uHpQ0th6QvgDKPv4AEnf8PI7RdAk=';
+            const encoder = new TextEncoder();
+            const uint8 = encoder.encode(data);
+            const hashBuffer = await crypto.subtle.digest('SHA-256', uint8);
+            const hashArray = Array.from(new Uint8Array(hashBuffer));
+            const actualHash = btoa(String.fromCharCode(...hashArray));
 
-		// Increment the load attempts
-		loadAttempts++;
+            if (`sha256-${actualHash}` !== expectedHash) {
+                console.error('Hash verification failed! Goodtube could not be loaded!')
+            }
 
-		// Get the GoodTube minified code
-		fetch('https://raw.githubusercontent.com/goodtube4u/goodtube/refs/heads/main/goodtube.min.js')
-			// Success
-			.then(response => response.text())
-			.then(data => {
-				// Load the GoodTube code into a <script> tag
-				let script = document.createElement('script');
-				script.innerHTML = data;
-				document.body.appendChild(script);
-			})
-			// Error
-			.catch((error) => {
-				// Try again after 500ms
-				setTimeout(function () {
-					goodTube_load(loadAttempts);
-				}, 500);
-			});
-	}
+            // Append script if hash matches
+            let script = document.createElement('script');
+            script.textContent = goodTube_csp.createScript(data);
+            document.head.appendChild(script);
 
-	// Let's go!
-	goodTube_load();
+        } catch (error) {
+            setTimeout(() => {
+                goodTube_load(loadAttempts);
+            }, 500);
+        }
+    }
+
+    // Let's go!
+    goodTube_load();
 })();
